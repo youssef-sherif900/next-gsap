99a2f6ac5b1b42a015144cb978df91cfdfdd9646
youssef-sherif900 <youssefsherif469@gmail.com>
2025-03-23 06:29:51 +0200

    updating open graph image

--- app/layout.js
+++ app/layout.js
@@ -22,7 +22,7 @@ export const metadata = {
     type: "website",
     images: [
       {
-        url: "/OG-Image.jpg",
+        url: "https://next-gsap-chi.vercel.app/OG-Image.jpg",
         width: 1200,
         height: 630,
         alt: "Trendy summer fashion and accessories",
